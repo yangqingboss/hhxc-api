b15f88de388889c1677eca0253fa3599bb41847f
hjboss <hongjiangproject@yahoo.com>
2015-12-12 17:57:40 +0800

    Initial commit

---
+++ config.mobile.php
@@ -0,0 +1,20 @@
+<?php
+// Copyright 2015 The Haohaoxiuche Team Authors. All right reserved.
+// Use of this source that is governed by a Apache-style
+// license that can be found in the LICENSE file.
+//
+// 配置數據庫和其他全局參數項，用於手機網頁環境
+//
+// @authors hjboss <hongjiangproject@yahoo.com> 2015-12-12#
+// @version 1.0.0
+// @package hlibc
+if (!defined('HHXC')) die('Permission denied');
+
+define('DEBUG',      FALSE);
+define('DB_HOST',    '127.0.0.1');
+define('DB_NAME',    'goview');
+define('DB_NAME_S',  'goview1');
+define('DB_USER',    'root');
+define('DB_PWD',     'goodluck123@');
+define('DB_CHARSET', 'GB2312');
+define('CL_CHARSET', 'UTF-8');
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+// Copyright 2015 The Haohaoxiuche Team Authors. All right reserved.
+// Use of this source that is governed by a Apache-style
+// license that can be found in the LICENSE file.
+//
+// 深圳市觀微科技有限公司 好好修車APP 目錄保護腳本
+//
+// @authors hjboss <hongjiangproject@yahoo.com> 2015-12-12#
+// @version 1.0.0
+// @package hhxc
+if (!defined('HHXC')) die('Permission denied');
+
