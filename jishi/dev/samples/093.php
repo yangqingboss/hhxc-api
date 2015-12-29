9488e5129e44b4a9ffff14ebac0a668f08188910
hjboss <hongjiangproject@yahoo.com>
2015-12-29 21:12:23 +0800

    Initial commit

---
+++ jishi/dev/samples/093.php
@@ -0,0 +1,34 @@
+<?php
+// Copyright 2015 The Haohaoxiuche Team Authors. All right reserved.
+// Use of this source that is governed by a Apache-style
+// license that can be found in the LICENSE file.
+//
+// 技術版API編號086 汽修人之信息盒子-@我的數據
+//
+// @authors hjboss <hongjiangproject@yahoo.com> 2015-12-14#
+// @version 1.0.0
+// @package hhxc
+$GLOBALS['DATA'] = array(
+	array(
+		'uid'         => 64,
+		'openid'      => '5622c82b715edfbd6dce666ccf7c9ca1',
+		'tid'         => 0,
+		'tag'         => 1,
+		'deviceid'    => 'a8e8814b47e6a5ecd',
+	),
+	array(
+		'uid'         => 64,
+		'openid'      => '5622c82b715edfbd6dce666ccf7c9ca1',
+		'tid'         => 0,
+		'tag'         => 3,
+		'deviceid'    => 'a8e8814b47e6a5ecd',
+	),
+	array(
+		'uid'         => 64,
+		'openid'      => '5622c82b715edfbd6dce666ccf7c9ca1',
+		'tid'         => 0,
+		'tag'         => 4,
+		'deviceid'    => 'a8e8814b47e6a5ecd',
+	),
+
+);
