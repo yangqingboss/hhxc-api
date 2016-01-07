39728bff879f4a6b673b6bcbe1159d36c4bb9aff
hjboss <hongjiangproject@yahoo.com>
2016-01-07 22:52:18 +0800

    Initial commit

---
+++ jishi/dev/samples/036.php
@@ -0,0 +1,17 @@
+<?php
+// Copyright 2015 The Haohaoxiuche Team Authors. All right reserved.
+// Use of this source that is governed by a Apache-style
+// license that can be found in the LICENSE file.
+//
+// 技術版API編號109 
+//
+// @authors hjboss <hongjiangproject@yahoo.com> 2015-12-14#
+// @version 1.0.0
+// @package hhxc
+$GLOBALS['DATA'] = array(
+	array(
+		'databasever' => 0,
+		'uid'         => 967,
+		'faultid'     => 0,
+	),
+);
